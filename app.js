eaf8dcd9532ca740c13851663c86a4effd16b0c5
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-04-07 22:03:55 +0330

    e.15

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ linkedList.prepend("first value");
 console.log(linkedList.toArray());
 */
 // ============================================== e.14 / Linked List - delete =====================================================
-
+/*
 class LinkedList {
 	constructor() {
 		this.head = null;
@@ -250,5 +250,110 @@ linkedList.delete("s");
 linkedList.delete("first value");
 linkedList.delete("first value");
 linkedList.delete(true);
+
+console.log(linkedList.toArray());
+*/
+// ============================================== e.15 / Linked List - find & insertAfter =====================================================
+
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	append(value) {
+		const newElement = { value: value, next: null };
+
+		if (this.tail) this.tail.next = newElement;
+
+		this.tail = newElement;
+
+		if (!this.head) this.head = newElement;
+	}
+
+	prepend(value) {
+		const newElement = { value: value, next: this.head };
+
+		this.head = newElement;
+
+		if (!this.tail) this.tail = newElement;
+	}
+
+	insertAfter(value, afterValue) {
+		const existingElement = this.find(afterValue);
+
+		if (existingElement) {
+			const newElement = { value: value, next: existingElement.value };
+
+			existingElement.next = newElement;
+		}
+	}
+
+	find(value) {
+		if (!this.head) return;
+
+		let currentElement = this.head;
+
+		while (currentElement) {
+			if (currentElement.value === value) return currentElement;
+
+			currentElement = currentElement.next;
+		}
+
+		return;
+	}
+
+	delete(value) {
+		if (!this.head) return;
+
+		while (this.head && this.head.value === value) this.head = this.head.next;
+
+		let currentElement = this.head;
+
+		while (currentElement.next) {
+			if (currentElement.next.value === value) currentElement.next = currentElement.next.next;
+			else currentElement = currentElement.next;
+		}
+
+		if (this.tail.value === value) this.tail = currentElement;
+	}
+
+	toArray() {
+		const element = [];
+
+		let currentElement = this.head;
+
+		while (currentElement) {
+			element.push(currentElement);
+
+			currentElement = currentElement.next;
+		}
+
+		return element;
+	}
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append(2);
+linkedList.append("s");
+linkedList.append("s");
+linkedList.append(true);
+
+linkedList.prepend("first value");
+linkedList.prepend("first value");
+
+linkedList.delete("s");
+linkedList.delete("s");
+linkedList.delete("first value");
+linkedList.delete("first value");
+linkedList.delete(true);
+
+console.log(linkedList.toArray());
+
+console.log(linkedList.find("s"));
+console.log(linkedList.find(2));
+
+linkedList.insertAfter("new value", 2);
 
 console.log(linkedList.toArray());
