da3a40405253ef11aea6677e831dd0fbb7bef2c0
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-01 23:25:50 +0330

    e.32

--- app.js
+++ app.js
@@ -573,7 +573,7 @@ function findFirstChild(str) {
 console.log(findFirstChild(message));
 */
 // ============================================== e.30 / hash table Implementation ==============================================================================
-
+/*
 class HashTable {
 	constructor() {
 		this.size = 1_000;
@@ -594,7 +594,7 @@ class HashTable {
 const message = "hello world";
 
 function findFirstChild(str) {
-  const table = new HashTable();
+	const table = new HashTable();
 
 	for (let char of str) {
 		if (table[char]) return char;
@@ -604,4 +604,48 @@ function findFirstChild(str) {
 }
 
 console.log(findFirstChild(message));
+*/
+// ============================================== e.31 / hash table Set & get ==============================================================================
+/*
+class HashTable {
+	constructor() {
+		this.size = 1_000;
+		this.products = Array(1_000).fill(null);
+	}
 
+	hash(key) {
+		let hash = 0;
+
+		for (const char of key) hash += char.charCodeAt(0);
+
+		return hash % this.size;
+	}
+
+	set(key, value) {
+		const keyHash = this.hash(key);
+
+		this.products[keyHash] = value;
+	}
+
+	get(key) {
+		const keyHash = this.hash(key);
+
+		return this.products[keyHash];
+	}
+}
+
+const message = "hello world";
+
+function findFirstChild(str) {
+	const table = new HashTable();
+
+	for (let char of str) {
+		if (table.get(char)) return char;
+
+		table.set(char, 1);
+	}
+}
+
+console.log(findFirstChild(message));
+*/
+// ============================================== e.32 / Tree ===================================================================================================
