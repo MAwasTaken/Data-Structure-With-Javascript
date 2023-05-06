435a8ddc7bf77944e047a3abd7fa44cbeec1476f
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-06 20:50:13 +0330

    e.37

--- app.js
+++ app.js
@@ -691,7 +691,7 @@ cartNodeData.node.addNode("Book 1");
 console.log(user);
 */
 // ============================================== e.36 / Tree Implementation Optimization ===================================================================================================
-
+/*
 class Node {
 	constructor(value, parentNode = null) {
 		this.childeren = [];
@@ -745,5 +745,85 @@ const user = new Tree("root");
 user.add("/userInfo/username/MAwasTaken");
 user.add("/cart/Book 1");
 user.add("/cart/Book 2");
+
+console.log(user);
+*/
+// ============================================== e.37 / Tree Implementation remove node ===================================================================================================
+
+class Node {
+	constructor(value, parentNode = null) {
+		this.childeren = [];
+		this.value = value;
+		this.parent = parentNode;
+	}
+
+	addNode(value) {
+		const segments = value.split("/");
+
+		if (segments.length === 0) return;
+		if (segments.length === 1) {
+			const node = new Node(segments[0], this);
+
+			this.childeren.push(node);
+
+			return { node: node, index: this.childeren.length - 1 };
+		}
+
+		const existingChildNode = this.childeren.find((item) => item.value === segments[0]);
+
+		if (existingChildNode) existingChildNode.addNode(segments.slice(1).join("/"));
+		else {
+			const node = new Node(segments[0], this);
+
+			node.addNode(segments.slice(1).join("/"));
+
+			this.childeren.push(node);
+
+			return { node: node, index: this.childeren.length - 1 };
+		}
+	}
+
+	removeNode(value) {
+		const segments = value.split("/");
+
+		if (segments.length === 0) return;
+		if (segments.length === 1) {
+			const existingNodeIndex = this.childeren.findIndex((item) => item.value === segments[0]);
+
+			if (existingNodeIndex < 0) throw new Error("Could not find!");
+
+			this.childeren.splice(existingNodeIndex, 1);
+		}
+		if (segments.length > 1) {
+			const existingChildNode = this.childeren.findIndex((item) => item.value === segments[0]);
+
+			if (!existingChildNode) throw new Error("Could not find!");
+
+			existingChildNode.removeNode(segments.slice(1).join("/"));
+		}
+	}
+}
+
+class Tree {
+	constructor(rootValue) {
+		this.root = new Node(rootValue);
+	}
+
+	add(path) {
+		this.root.addNode(path);
+	}
+
+	remove(path) {
+		this.root.removeNode(path);
+	}
+}
+
+const user = new Tree("root");
+
+user.add("/userInfo/username/MAwasTaken");
+user.add("/cart/Book 1");
+user.add("/cart/Book 2");
+
+user.remove("/cart/Book 2");
 
 console.log(user);
