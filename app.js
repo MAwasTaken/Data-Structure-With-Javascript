d52bb9ea3c38fdaa3c1b2c4275e8c5d1f2f5af33
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-13 21:07:36 +0330

    e.39

--- app.js
+++ app.js
@@ -829,3 +829,100 @@ user.remove("/cart/Book 2");
 console.log(user);
 */
 // ============================================== e.38 / Search in Tree ===================================================================================================
+// ============================================== e.39 / Depth First Tree ===================================================================================================
+
+class Node {
+	constructor(value, parentNode = null) {
+		this.childeren = [];
+		this.value = value;
+		this.parent = parentNode;
+	}
+
+	addNode(value) {
+		const segments = value.split("/");
+
+		if (segments.length === 0) return;
+		if (segments.length === 1) {
+			const node = new Node(segments[0], this);
+
+			this.childeren.push(node);
+
+			return { node: node, index: this.childeren.length - 1 };
+		}
+
+		const existingChildNode = this.childeren.find((item) => item.value === segments[0]);
+
+		if (existingChildNode) existingChildNode.addNode(segments.slice(1).join("/"));
+		else {
+			const node = new Node(segments[0], this);
+
+			node.addNode(segments.slice(1).join("/"));
+
+			this.childeren.push(node);
+
+			return { node: node, index: this.childeren.length - 1 };
+		}
+	}
+
+	removeNode(value) {
+		const segments = value.split("/");
+
+		if (segments.length === 0) return;
+		if (segments.length === 1) {
+			const existingNodeIndex = this.childeren.findIndex((item) => item.value === segments[0]);
+
+			if (existingNodeIndex < 0) throw new Error("Could not find!");
+
+			this.childeren.splice(existingNodeIndex, 1);
+		}
+		if (segments.length > 1) {
+			const existingChildNode = this.childeren.findIndex((item) => item.value === segments[0]);
+
+			if (!existingChildNode) throw new Error("Could not find!");
+
+			existingChildNode.removeNode(segments.slice(1).join("/"));
+		}
+	}
+
+	find(value) {
+		for (const item of this.childeren) {
+			if (item.value === value) return item;
+
+			const nestedChildNode = item.find(value);
+
+			if (nestedChildNode) return nestedChildNode;
+		}
+	}
+}
+
+class Tree {
+	constructor(rootValue) {
+		this.root = new Node(rootValue);
+	}
+
+	add(path) {
+		this.root.addNode(path);
+	}
+
+	remove(path) {
+		this.root.removeNode(path);
+	}
+
+	find(value) {
+		if (this.root.value === value) return this.root;
+
+		return this.root.find(value);
+	}
+}
+
+const user = new Tree("root");
+
+user.add("/userInfo/username/MAwasTaken");
+user.add("/cart/Book 1");
+user.add("/cart/Book 2");
+
+user.remove("/cart/Book 2");
+
+console.log(user.find("username"));
+
+console.log(user);
