bbd14d9cf2b8d779b7e5e2e4398a93da1fcf615b
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-06-30 19:29:54 +0330

    e.53

--- app.js
+++ app.js
@@ -1262,7 +1262,7 @@ console.log(tree);
 // ============================================== e.50 / graph in code ==============================================================================
 // ============================================== e.51 / list & matrix ==============================================================================
 // ============================================== e.52 / graph structure ==============================================================================
-
+/*
 class Graph {
 	constructor() {
 		this.node = {};
@@ -1289,5 +1289,58 @@ graph.addNode(3, 'Book 3');
 graph.addEdge(1, 2);
 graph.addEdge(1, 3);
 graph.addEdge(3, 2);
+
+console.log(graph);
+*/
+// ============================================== e.53 / graph remove node ==============================================================================
+
+class Graph {
+	constructor() {
+		this.node = {};
+		this.edge = {};
+	}
+
+	addNode(id, value) {
+		this.node[id] = value;
+	}
+
+	addEdge(startNode, endNode) {
+		if (this.edge[startNode] && this.edge[startNode].indexOf(endNode) === -1)
+			this.edge[startNode].push(endNode);
+		else this.edge[startNode] = [endNode];
+	}
+
+	removeNode(nodeId) {
+		this.node[nodeId] = undefined;
+
+		Reflect.deleteProperty(this.edge, nodeId);
+
+		for (const edgeId in this.edge) {
+			let i = 0;
+
+			for (const endNode of this.edge[edgeId]) {
+				if (endNode === nodeId) {
+					this.edge[edgeId].splice(i, 1);
+
+					break;
+				}
+
+				i++;
+			}
+		}
+	}
+}
+
+const graph = new Graph();
+
+graph.addNode(1, 'Book 1');
+graph.addNode(2, 'Book 2');
+graph.addNode(3, 'Book 3');
+
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(3, 2);
+
+graph.removeNode(2);
 
 console.log(graph);
